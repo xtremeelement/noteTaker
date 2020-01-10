466855b1378875274fd33f9e47ea14cc19cf85b7
Brandon Downhour <xtremeelement@gmail.com>
2020-01-09 22:07:54 -0500

    Started creating fs readfile and writefile functions

--- public/assets/js/apistuff.js
+++ public/assets/js/apistuff.js
@@ -1,20 +1,42 @@
 const fs = require("fs");
 const path = require("path");
-const counter = 0;
+let counter;
 
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
-    res.json(fs.readFile("..../db/db.json"));
+    res.json("../../../db/db.json");
   });
 
   app.post("/api/notes", function(req, res) {
-    const test = {
-      name: "blah"
+    const dbData = JSON.parse(fs.readFile("../../../db/db.json"));
+
+    counter = dbData.length + 1;
+
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: counter
     };
 
-    test.id = counter;
+    if (!newNote.name || !newNote.text) {
+      return res.status(400).json({ msg: "please fill title and text" });
+    }
+
+    dbData.push(newNote);
+
+    dbData = JSON.stringify(dbData);
+
+    fs.writeFile("../../../db/db.json", data, function(err) {
+      if (err) throw err;
+      console.log("Wrote to file");
+    });
+
+    Json.stringify(fs.writeFile("../../../db/db.json"));
+
     counter++;
   });
 
-  app.delete("/api/notes/:id", (req, res) => {});
+  app.delete("/api/notes/:id", (req, res) => {
+    const deleteNote = req.body;
+  });
 };
